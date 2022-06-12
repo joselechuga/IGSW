8f5d8aac31f9d4cab1ecdd21fb962c57a7b0d1ae
joselechuga <jos.medinav@duocuc.cl>
2022-06-12 16:35:14 -0400

    productos y enlaces url

--- proyectoSw/core/urls.py
+++ proyectoSw/core/urls.py
@@ -1,7 +1,11 @@
 from importlib.resources import path
 from django.urls import URLPattern, path
-from .views import index
+from .views import index ,log,mousepad,com_tec
 
 urlpatterns=[
     path('',index,name="index"),
+    path('login/',log,name="login"),
+    path('mousepadRazer/',mousepad,name="mousepad"),
+    path('combo-teclado/',com_tec,name="combo_teclado"),
+
 ]
--- proyectoSw/core/views.py
+++ proyectoSw/core/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'core/index.html')
+
+def log(request):
+    return render(request,'core/login.html')
+
+def mousepad(request):
+    return render(request,'core/mousepad.html')
+
+def com_tec(request):
+    return render(request,'core/combo_teclado.html')
