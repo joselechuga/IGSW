1dd4058796bf27e695062715d595cea1665a016c
thepineapple1211 <jor.pina@duocuc.cl>
2022-06-13 12:23:03 -0400

    conexion a base de datos inicio login

--- proyectoSw/core/models.py
+++ proyectoSw/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class nuevoUsuario(models.Model):
+    nombreUsuario = models.CharField(max_length=50)
+    psw = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    edad=models.IntegerField()
+    genero=models.CharField(max_length=1)
+
