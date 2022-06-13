44cbcbb37069cdafac2e10ed850c9f1fd81a3d32
thepineapple1211 <jor.pina@duocuc.cl>
2022-06-13 12:57:12 -0400

    jordan

--- proyectoSw/core/models.py
+++ proyectoSw/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class nuevoUsuario(models.Model):
-    nombreUsuario = models.CharField(max_length=60)
+    nombreUsuario = models.CharField(max_length=50)
     psw = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     edad=models.IntegerField()
