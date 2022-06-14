1aaae4429165dfc31b5b236f65d55a8e0790ff95
thepineapple1211 <jor.pina@duocuc.cl>
2022-06-13 20:27:18 -0400

    base de datos

--- proyectoSw/core/urls.py
+++ proyectoSw/core/urls.py
@@ -1,11 +1,13 @@
 from importlib.resources import path
 from django.urls import URLPattern, path
 from .views import index ,log,mousepad,com_tec
+from . import views
 
 urlpatterns=[
     path('',index,name="index"),
     path('login/',log,name="login"),
     path('mousepadRazer/',mousepad,name="mousepad"),
     path('combo-teclado/',com_tec,name="combo_teclado"),
+    path('registrarse', views.registroUsuario,name="reg"),
 
 ]
--- proyectoSw/core/views.py
+++ proyectoSw/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from core.models import nuevoUsuario
 # Create your views here.
 
 def index(request):
@@ -13,3 +14,16 @@ def mousepad(request):
 
 def com_tec(request):
     return render(request,'core/combo_teclado.html')
+
+def registroUsuario(request):
+    if request.method== 'POST':
+        nombreUsuario=request.POST.get('nombreUsuario')
+        psw=request.POST.get('password')
+        email=request.POST.get('correo')
+        edad=request.POST.get('edad')
+        genero=request.POST.get('genero')
+        nuevoUsuario(nombreUsuario=nombreUsuario,psw=psw,email=email,edad=edad,genero=genero).save()
+        messages.success(request,'el usuario'+request.POST['nombreUsuario']+'se ha registrado correctamente')
+        return render(request,'core/registrarse.html')
+    else:
+        return render(request,'core/registrarse.html')
